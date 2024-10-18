243a94f72b6d5d8dddd1d2119882c52dc8c9a9da
Suelen regina <suelen.regina.08@email.com>
2024-10-17 22:26:17 -0300

    atualizações

--- js/sw.js
+++ js/sw.js
@@ -1,52 +1,67 @@
 const CACHE_NAME = 'coffo-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/contact.html',
-    '/coffees.html',
-    '/blog.html',
-    '/about.html',
-    '/slick.css',
-    '/styles.css',
-    '/responsive.css',
-    '/app.js',
-    '/manifest.json',
-    '/icon-192x192.png',
-    '/icon-512x512.png'
+  '/',
+  '/index.html',
+  '/blog.html',
+  '/coffees.html',
+  '/contact.html',
+  '/about.html',
+  '/css/animate.min.css',
+  '/css/bootstrap-grid.css.map',
+  '/css/nice-select.css',
+  '/css/normalize.css',
+  '/css/responsive.css',
+  '/css/slick.css',
+  '/css/style.css',
+  '/css/meanmenu.css',
+  '/css/jquery-ui.css',
+  '/css/icomoon.css',
+  '/css/bootstrap-grid.min.css',
+  '/css/icomoon.css',
+  '/js/custom.js',
+  '/js/jquery.min.js',
+  '/js/modernizer.js',
+  '/js/plugin.js',
+  '/js/popper.min.js',
+  '/js/slider-setting.js',
+  '/js/sw.js',
+  '/manifest.json',
+  '/icons/icon-196x196.png',
+  '/icons/icon-512x512.png'
 ];
 
-// Instalando o service worker e cacheando arquivos
+// Instalando o Service Worker e armazenando em cache
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-// Respondendo com recursos do cache
+// Interceptando requisições para servir conteúdo do cache
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
 
-// Atualizando o cache
+// Atualizando o cache quando o Service Worker é ativado
 self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
